4483d975b9b60be54664b5e4675f8ef34dc8d533
Bastien <bastien.jean.dev@gmail.com>
2023-07-24 10:50:07 +0200

    fix VueMultiselect component not exported

--- src/multiselect/index.ts
+++ src/multiselect/index.ts
@@ -1,3 +1,13 @@
-export * from './globalExtensions'
-export { createMultiselect } from './multiselect'
-export * from './components/VueMultiselect.vue'
+import VueMultiselect from './components/VueMultiselect.vue'
+import { createMultiselect } from './multiselect'
+
+export {
+  VueMultiselect,
+  createMultiselect
+}
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    VueMultiselect: typeof VueMultiselect
+  }
+}
